70bb37c750e0643bf53b2765726cbb0ef5c3f3b3
Elliott Slaughter <slaughter@cs.stanford.edu>
2020-04-22 11:48:55 -0700

    Fix distro check.

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,7 @@ set -e
 
 IFS=- read distro release <<< "$1"
 
-if [[ $distro = 14.04 ]]; then
+if [[ $distro = ubuntu && $release = 14.04 ]]; then
     docker build --build-arg release=$release -t terralang/terra:$distro-$release -f docker/Dockerfile.$distro-upstream .
 else
     docker build --build-arg release=$release -t terralang/terra:$distro-$release -f docker/Dockerfile.$distro .
