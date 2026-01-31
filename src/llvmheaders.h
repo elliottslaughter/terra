8cf899fa7a0284f220ebb9a382f1d8c1f1fbf1e9
Elliott Slaughter <slaughter@cs.stanford.edu>
2026-01-30 22:25:47 -0800

    Add headers for LLVM 20.

--- src/llvmheaders.h
+++ src/llvmheaders.h
@@ -72,11 +72,13 @@
 #include "llvmheaders_180.h"
 #elif LLVM_VERSION < 200
 #include "llvmheaders_190.h"
+#elif LLVM_VERSION < 210
+#include "llvmheaders_200.h"
 #else
 #error "unsupported LLVM version"
 // for OSX code completion
-#define LLVM_VERSION 180
-#include "llvmheaders_180.h"
+#define LLVM_VERSION 200
+#include "llvmheaders_200.h"
 #endif
 
 #define UNIQUEIFY(T, x) (std::unique_ptr<T>(x))
